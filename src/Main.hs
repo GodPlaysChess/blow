ca5a0ebef707360cfd4500abd3b3e448e924573b
gparakhonskiy <gparakhonskiy@ebay.com>
2018-03-12 20:54:41 +0100

    added serialization library

--- src/Main.hs
+++ src/Main.hs
@@ -2,11 +2,10 @@
 
 module Main where
 
-import           Data.Text.Lazy         as T (pack)
-import           NLP.Hext.NaiveBayes    (emptyModel, runBayes, teach)
-import           Probability.Classifier (classifiedDocs)
+import           Data.Text.Lazy         as T (Text, pack)
+import           NLP.Hext.NaiveBayes    (BayesModel, emptyModel, runBayes, teach)
+import           Probability.Classifier (Class, classifiedDocs)
 import           Web.Scotty
-
 
 
 main :: IO ()
@@ -24,3 +23,20 @@ learningMain = do
   review <- readFile "./dist/resources/review1.txt"
   let result = runBayes (material classifiedDocs) review
   putStrLn $ "The review '" ++ review ++ "' is " ++ show result
+
+
+storagePath :: FilePath
+storagePath = "./dist/resources/modelstorage/bayesmodel"
+
+readModel :: IO (BayesModel Class)
+readModel = undefined
+
+writeModel :: BayesModel Class -> IO ()
+writeModel = undefined -- writeFile $ encode 
+
+-- delete them in the end. They are here just to memoize faster what's going on.
+updateModel :: T.Text -> Class -> BayesModel Class -> BayesModel Class
+updateModel = teach
+
+classify :: BayesModel Class -> String -> Class 
+classify = runBayes 
