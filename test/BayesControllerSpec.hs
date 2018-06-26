6f75f0c5ad8a9bdd97839a077d1130de0aae4927
Gleb <parakhonskiyg@gmail.com>
2018-06-26 20:58:35 +0200

    cleanup test

--- test/BayesControllerSpec.hs
+++ test/BayesControllerSpec.hs
@@ -2,34 +2,34 @@
 
 module BayesControllerSpec(spec) where
 
-import           BayesController               (initializeModelT, readModelT,
-                                                refineModelT, classifyModelT)
-import           Control.Exception.Base        (bracket_, finally)
-import           Control.Monad.Trans.Reader    (ReaderT, ask, local, runReaderT)
-import           Data.Either                   (Either (Right), isRight)
-import           System.Directory              (removeFile)
-import           System.IO.Unsafe              (unsafePerformIO)
+import           BayesController            (classifyModelT, initializeModelT,
+                                             readModelT, refineModelT)
+import           Control.Exception.Base     (bracket_)
+import           Control.Monad.Trans.Reader (runReaderT)
+import           Data.Bitraversable         (bitraverse)
+import           Data.Either                (isRight)
+import           Probability.Classifier
+import           System.Directory           (removeFile)
 import           Test.Hspec
-import          Probability.Classifier
-import           Data.Bitraversable(bitraverse)
 
 
 spec :: Spec
 spec = around_ withModelInitialised $ do
     describe "Controller" $ do
         it "initialize model should create a file with parsable model" $
-                         (runReaderT readModelT testModelStorage)  >>= (`shouldSatisfy` isRight)
+                         runReaderT readModelT testModelStorage  >>= (`shouldSatisfy` isRight)
 
         it "after refinement the model shall be classified correcly" $
-                     (runReaderT (do 
-                        _ <- refineModelT "Love story" Positive
-                        _ <- refineModelT "Horror story" Negative
+                     runReaderT (do
+                        refineModelT "Love story" Positive
+                        refineModelT "Horror story" Negative
                         bitraverse classifyModelT classifyModelT ("Love poem" , "horror poem"))
-                        testModelStorage) >>= (`shouldBe` (Positive, Negative))
+                        testModelStorage >>= (`shouldBe` (Positive, Negative))
+
 
 withModelInitialised :: IO () -> IO ()
 withModelInitialised        = bracket_ beforeTestInitialise
-                                       afterTestDelete                                        
+                                       afterTestDelete
 
 testModelStorage :: FilePath
 testModelStorage = "./test/resources/modelstorage"
@@ -38,7 +38,6 @@ beforeTestInitialise :: IO ()
 beforeTestInitialise = runReaderT
                             (initializeModelT "./test/resources/initial_training_set")
                             testModelStorage
-
 
 afterTestDelete :: IO ()
 afterTestDelete = removeFile testModelStorage
